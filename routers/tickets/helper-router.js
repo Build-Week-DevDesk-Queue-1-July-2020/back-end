ee95486176ce50bbcc9895c60f571ed682e25d94
John Oblenda <joandong2@gmail.com>
2020-07-29 19:37:52 -0400

    cleaning up routes

--- routers/tickets/helper-router.js
+++ routers/tickets/helper-router.js
@@ -3,51 +3,63 @@ const Tickets = require("./helper-model.js");
 const router = express.Router();
 
 router.put("/:id/tickets/:ticketId/:status", (req, res, next) => {
-    Tickets.findById(req.params.ticketId)
-        .then((ticket) => {
-            if (ticket) {
-                let statusText = "",
-                    helper_id;
+    if (
+        req.params.status == "completed" ||
+        req.params.status == "inprogress" ||
+        req.params.status == "open"
+    ) {
+        Tickets.findById(req.params.ticketId)
+            .then((ticket) => {
+                if (ticket) {
+                    let statusText = "",
+                        helper_id;
 
-                switch (req.params.status) {
-                    case "completed":
-                        statusText = "completed";
-                        helper_id = req.params.id;
-                        break;
-                    case "inprogress":
-                        statusText = "inprogress";
-                        helper_id = req.params.id;
-                        break;
-                    case "open":
-                        statusText = "open";
-                        helper_id = 0;
-                        break;
-                    default:
-                        statusText = ticket.status;
-                        helper_id = ticket.helper_id;
-                        break;
-                }
+                    switch (req.params.status) {
+                        case "completed":
+                            statusText = "completed";
+                            helper_id = req.params.id;
+                            break;
+                        case "inprogress":
+                            statusText = "inprogress";
+                            helper_id = req.params.id;
+                            break;
+                        case "open":
+                            statusText = "open";
+                            helper_id = 0;
+                            break;
+                        default:
+                            statusText = ticket.status;
+                            helper_id = ticket.helper_id;
+                            break;
+                    }
 
-                Tickets.update(
-                    {
-                        helper_id: helper_id,
-                        status: statusText,
-                    },
-                    req.params.ticketId
-                ).then((ticket) => {
-                    res.json({
-                        message: `Successfully updated ticket.`,
+                    Tickets.update(
+                        {
+                            helper_id: helper_id,
+                            status: statusText,
+                        },
+                        req.params.ticketId
+                    ).then((ticket) => {
+                        res.json({
+                            message: `Successfully updated ticket.`,
+                        });
                     });
+                } else {
+                    res.status(404).json({
+                        message: "Could not assigned ticket",
+                    });
+                }
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    message: "Having issues with the server.",
                 });
-            } else {
-                res.status(404).json({
-                    message: "Could not assigned ticket",
-                });
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({ message: "Having issues with the server." });
+            });
+    } else {
+        res.status(404).json({
+            message: "No tickets related to that status.",
         });
+    }
 });
 
 router.get("/tickets", (req, res) => {
